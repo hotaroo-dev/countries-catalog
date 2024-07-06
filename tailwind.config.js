9f8220745dea5e308546934428b119d53b793490
hotaroo-dev <kay.sothearo2019@rupp.edu.kh>
2024-07-06 10:26:42 +0700

    feat(*): project setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "1.25rem",
+        lg: "1.5rem",
+      },
+    },
+  },
+  plugins: [],
+};
